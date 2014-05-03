e6e019a4dad3e374e1cf42e4591197a04a7fec50
Nikola Kadiev <nikola.kadiev@gmail.com>
2014-05-03 13:34:39 +0200

    Added logging and page-redirecting for various exceptions.Alsoadded some basic input validation.

--- src/newsReader/Find.java
+++ src/newsReader/Find.java
@@ -1,81 +1,92 @@
 package newsReader;
 
 import java.io.IOException;
-
-import newsReader.UrlInfo;
-
-//import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
+import newsReader.UrlInfo;
 
 import com.google.appengine.labs.repackaged.org.json.JSONArray;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 @SuppressWarnings("serial")
-public class Find extends HttpServlet {
-	ArrayList<String> links = new ArrayList<String>();
+public class Find extends HttpServlet 
+{	
+	private static final Logger loger = Logger.getLogger(Find.class.getName());
+	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException 
   {
-		String url;
-		resp.setContentType("text/plain");
-		url = req.getParameter("searchText");
 		
-		UrlInfo info = new UrlInfo(url);
-       //PrintWriter out = resp.getWriter();
-        
-       // out.println(info.getTitle());
-       // out.println(info.getDescription());
-        
-       // for(String word : info.getKeywords())
-      //  {
-      //  out.println(word);
-      //  }
-        
-        //for(WordOccurence word : info.orderedValues )
-       // {
-      //  out.println(word.key + ":" + word.count);	
-      //  }
-        
-          
-       try 
-       {
-    	    JSONObject  jsonObject = info.searchFeeds(req.getRemoteAddr());
-		    JSONObject  responseData = jsonObject.getJSONObject("responseData");
-		    JSONArray  entries = responseData.getJSONArray("entries");
+	 resp.setContentType("text/plain");
+	 
+	 //validate form input entered by the user 
+	 if(req.getParameter("searchText") != null || !req.getParameter("searchText").equals(""))
+	 {	
+	  try
+	  { 
+		  ArrayList<String> links = new ArrayList<String>();
+		  String urlAddress = req.getParameter("searchText");
+		
+		  //only used to throw MalformedURLException
+		  //if the input String urlAddress is not a valid url address
+		  @SuppressWarnings("unused")
+		  URL testUrlValidity = new URL(urlAddress);
+		
+		  UrlInfo info = new UrlInfo(urlAddress);
+		 
+		  //access the Google Feeds API(using the searchFeeds method) and get the response data
+		  JSONObject  jsonObject = info.searchFeeds(req.getRemoteAddr());
+		  JSONObject  responseData = jsonObject.getJSONObject("responseData");
+		  JSONArray  entries = responseData.getJSONArray("entries");
+		
+		  //loop through the entries array and get the url from each JSON object
+		  //and add it to the links list
+		  for(int i=0;i<entries.length();i++)
+		  	{
+			  	JSONObject obj = entries.getJSONObject(i);
+			  	links.add( obj.getString("url"));
+		  	}
 		    
-		    for(int i=0;i<entries.length();i++)
-		    {
-		    	JSONObject obj = entries.getJSONObject(i);
-		    	links.add( obj.getString("url"));
-		    }
-		    
-       }
-	   catch (JSONException e) 
-		    {
-             e.printStackTrace();
-	        }
- 
-    	req.setAttribute("FeedLinks", links);
+       
+		  req.setAttribute("FeedLinks", links);
     	
-    	try {
-    		HttpSession session = req.getSession();
-    		session.setAttribute("FeedLinks", links);
-    		session.setAttribute("UrlInfo", info);
-			req.getRequestDispatcher("viewLinks.jsp").forward(req, resp);
-			
-			url = null;
-		} catch (ServletException e) {
-			
-			e.printStackTrace();
-		}
     	
-       int i;
+    	
+		  HttpSession session = req.getSession();
+		  session.setAttribute("FeedLinks", links);
+		  session.setAttribute("UrlInfo", info);
+		  req.getRequestDispatcher("viewLinks.jsp").forward(req, resp);
+    	   
+     }
+		
+	catch(JSONException e)
+    {
+		loger.log(Level.SEVERE, "JSONException", e);
+		resp.sendRedirect("errorPage.html");
+    }
+		 
+    catch(ServletException e)
+    {
+    	loger.log(Level.SEVERE, "ServletException", e);
+    	resp.sendRedirect("errorPage.html");    
+    }
+		 
+	catch(MalformedURLException e)
+	{
+		loger.log(Level.SEVERE, "MalformedUrl", e);
+		resp.sendRedirect("errorPage.html");	  
+	}
+    	
+    	
+   }
         
- }
+  }
 	
 	
 }
