14e976d8d2e68927167aa0ade181b8de652d1958
Nikola Kadiev <nikola.kadiev@gmail.com>
2014-05-03 20:51:55 +0200

    Added methods documentation and comments

--- src/newsReader/FeedParser.java
+++ src/newsReader/FeedParser.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,10 +13,23 @@ import org.jsoup.nodes.Element;
 import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
 
+/**
+ * Simple utility class used for extracting data from web feeds.
+ * @author Nikola Kadiev
+ *
+ */
 public class FeedParser 
 {
+	private static final Logger loger = Logger.getLogger(Find.class.getName());
 	
-	
+	/**
+	 * Traverses the JSOUP XML Document(Rss or Atom feed) selects each individual feed entry element
+	 * and fills a ArrayList with the text values of title,link and description elements.
+	 * The ArrayList is filled using a simple FeedEntry object(plain container for the extracted String values)
+	 * @param url  web Feed Url Address
+	 * @return ArrayList(FeedEntry) - Each FeedEntry object has getters for title,link and description
+	 * @throws IOException
+	 */
 	public  ArrayList<FeedEntry> getFeedEntries(String url) throws IOException
 	{
 		ArrayList<FeedEntry> webSyndicationItems = new ArrayList<FeedEntry>();
@@ -25,50 +40,57 @@ public class FeedParser
 		FeedEntry rssEntry, atomEntry;
 		
 		
-			//process RSS feeds
-			if(xmlDoc.select("rss") != null)
+		//process RSS feeds
+		if(xmlDoc.select("rss") != null)
+		{
+			//get all item elements
+			Elements rssItemElements  = xmlDoc.select("item");
+				
+			//fill FeedEntry objects
+			for(Element e : rssItemElements)
 			{
-			   
-			   Elements itemElements  = xmlDoc.select("item");
-			   
-			   for(int i = 0; i < itemElements.size();i++)
-			   {
-				   Element e = itemElements.get(i);
+				//Element e = itemElements.get(i);
+				    
+				rssEntry = new FeedEntry();
 				   
+				rssEntry.setTitle(e.getElementsByTag("title").text());
+				rssEntry.setLink(e.getElementsByTag("link").text());
+				rssEntry.setDescription(e.getElementsByTag("description").text());
 				   
-				   rssEntry = new FeedEntry();
-				   
-				   rssEntry.setTitle(e.getElementsByTag("title").text());
-				   rssEntry.setLink(e.getElementsByTag("link").text());
-				   rssEntry.setDescription(e.getElementsByTag("description").text());
-				   
-				   webSyndicationItems.add(rssEntry);
-			   }
-			   
+				webSyndicationItems.add(rssEntry);
 			}
+			   
+		}
 			
-			//process ATOM feeds
-		    if((root = xmlDoc.select("feed")) != null && root.attr("xmlns").contains("http://www.w3.org/2005/Atom"))
-			{
-			  Elements atomEntries = xmlDoc.select("entry");
-			   
-			    for(Element e : atomEntries)
-			   {
-			      atomEntry = new FeedEntry();
+		//process ATOM feeds
+		if((root = xmlDoc.select("feed")) != null && root.attr("xmlns").contains("http://www.w3.org/2005/Atom"))
+		{
+		    //get all entry elements	
+		    Elements atomEntryElements = xmlDoc.select("entry");
+		    		
+		    //fill FeedEntry objects
+		    for(Element e : atomEntryElements)
+		    {
+		    	atomEntry = new FeedEntry();
 			      
-			      atomEntry.setTitle(e.getElementsByTag("title").text());
-			      atomEntry.setDescription(e.getElementsByTag("summary").text());
-			      atomEntry.setLink(e.getElementsByTag("link").attr("href"));
+		    	atomEntry.setTitle(e.getElementsByTag("title").text());
+		    	atomEntry.setDescription(e.getElementsByTag("summary").text());
+		    	atomEntry.setLink(e.getElementsByTag("link").attr("href"));
 				 
-				  webSyndicationItems.add(atomEntry); 
-			   }	
-			}
+		    	webSyndicationItems.add(atomEntry); 
+		    }	
+		}
 			
 			
 	 return webSyndicationItems;
 	}
 	
-	
+	/**
+	 * Gets the entire markup content of the provided url. 
+	 * @param inputUrl provides the target address 
+	 * @return String containing the whole page markup code 
+	 * @throws IOException
+	 */
 	private String getHTML(String inputUrl) throws IOException
 	{
 		StringBuilder builder = new StringBuilder();
@@ -76,30 +98,30 @@ public class FeedParser
 		
 		try
 		{
-	   	  url = new URL(inputUrl);
-	   	  URLConnection conn = url.openConnection();
-	   	  conn.setReadTimeout(10000);
-	      BufferedReader in = new BufferedReader(
-	      new InputStreamReader(conn.getInputStream()));
+			url = new URL(inputUrl);
+			URLConnection conn = url.openConnection();
+			conn.setReadTimeout(10000);
+			BufferedReader in = new BufferedReader(
+			new InputStreamReader(conn.getInputStream()));
 	      
-	      String line;
+	        String line;
 	      
 	        while ((line = in.readLine()) != null)
 	         {
-	          builder.append(line);
+	        	builder.append(line);
 	         }
 	        
 	        in.close();
 	   	}
 		
-		catch(MalformedURLException e)
+			catch(MalformedURLException e)
 		{
-		 e.printStackTrace();
+			loger.log(Level.SEVERE, "MalformedUrl", e);	  
 		}
 	   	  
 	   	 
 	        
-	        return builder.toString();
+	 return builder.toString();
 	}
 	
 	
